717d00f69be7a23dd6933a5ea793681212ddf319
MoritzSchwarz <moritz.schwarz148@gmx.de>
2025-05-13 10:54:57 +0200

    added bash-script for 30 measurements per bpmn

--- gmt-deployment.sh
+++ gmt-deployment.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-# Dein Token
 GMT_TOKEN='guihe_ASju4Fsa-FAshzh3zFH))afhhz'
 
-# JSON-Payload
 json_body='{"name":"C8-complex-long-test","email":"moritz.schwarz@envite.de","image_url":"","repo_url":"https://github.com/Moritz148/camunda8experiment.git","filename":"","branch":"complex_long","machine_id":"6","schedule_mode":"one-off"}'
 
-# Einmaliger POST-Request
+# Submit 30 times to GMT
+for ((i=1; i<=30; i++)); do
 curl 'https://api.green-coding.io/v1/software/add' \
   -H 'Origin: https://metrics.green-coding.io/' \
   -H "X-Authentication: ${GMT_TOKEN}" \
   -H 'Content-Type: application/json' \
   --data-raw "$json_body"
 
-echo "Einmaliger POST ausgeführt"
+   echo "Iteration $i"
+done
+
