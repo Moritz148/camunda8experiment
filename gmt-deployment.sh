3bc76a508c13b2bef9efdb70617a944a51920917
MoritzSchwarz <moritz.schwarz148@gmx.de>
2025-05-13 11:25:16 +0200

    added bash-script for 30 measurements per bpmn

--- gmt-deployment.sh
+++ gmt-deployment.sh
@@ -2,7 +2,7 @@
 
 GMT_TOKEN='guihe_ASju4Fsa-FAshzh3zFH))afhhz'
 
-json_body='{"name":"C8-complex-long-test","email":"moritz.schwarz@envite.de","image_url":"","repo_url":"https://github.com/Moritz148/camunda8experiment.git","filename":"","branch":"complex_long","machine_id":"6","schedule_mode":"one-off"}'
+json_body='{"name":"C8-long_complex_final","email":"moritz.schwarz@envite.de","image_url":"","repo_url":"https://github.com/Moritz148/camunda8experiment.git","filename":"","branch":"complex_long","machine_id":"6","schedule_mode":"one-off"}'
 
 # Submit 30 times to GMT
 for ((i=1; i<=30; i++)); do
